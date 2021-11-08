a4ab2d5b6c03e68a01afd8e82598ab45bc263973
nikbit <85894877+nikbit@users.noreply.github.com>
2021-11-08 09:10:13 +0100

    attivazione a luminosità elevata per spegnere le luci

--- main.ts
+++ main.ts
@@ -1,8 +1,21 @@
+let luminosità = 0
 basic.forever(function () {
-    basic.showNumber(input.lightLevel())
+    luminosità = input.lightLevel()
+    basic.showNumber(luminosità)
     led.plotBarGraph(
-    input.lightLevel(),
+    luminosità,
     255
     )
     basic.pause(2000)
+    if (luminosità > 150) {
+        basic.showLeds(`
+            # # # # .
+            . # . # .
+            . # # # .
+            . # # # .
+            . . # . #
+            `)
+        music.playTone(262, music.beat(BeatFraction.Double))
+        basic.showString("luci off")
+    }
 })
